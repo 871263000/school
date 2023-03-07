d653678732e0d2fa64837f4132cc193a80009ecc
zdl <871263000@qq.com>
2023-03-07 13:41:47 +0800

    修改路由写法

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\HTTP\Controller\SchoolController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post("/add_school", "SchoolController@index");
+Route::post("/add_school", [SchoolController::class, 'index']);
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
