a26d9d6c777c23dabf1fe1feafbb9195cc5bb00c
zdl <871263000@qq.com>
2023-03-10 21:10:07 +0800

    数据库密码

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '47.92.38.81'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'school'),
             'username' => env('DB_USERNAME', 'root'),
